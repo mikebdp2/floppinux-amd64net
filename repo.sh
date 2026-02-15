8d1f2078bcbff2d3f6ca69a69a716cfb2d072285
Mike Banon <mikebdp2@gmail.com>
2026-02-15 12:11:34 +0000

    Add files via upload

---
+++ repo.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+wget "https://raw.githubusercontent.com/mikebdp2/floppinux-amd64net/refs/heads/main/kirc"
+wget "https://raw.githubusercontent.com/mikebdp2/floppinux-amd64net/refs/heads/main/kirc.txt"
+chmod +x ./kirc
+#

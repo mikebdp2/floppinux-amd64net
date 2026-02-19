06a2c7ba4d179df27ec1e6ce69d1055018c6288c
Mike Banon <mikebdp2@gmail.com>
2026-02-19 02:32:35 +0000

    Update floppinux_amd64net.sh
    comment out welcome message for now, disappointed at reproducible build size inflation

--- floppinux_amd64net.sh
+++ floppinux_amd64net.sh
@@ -503,7 +503,7 @@ busybox_build () {
     mkdir -p {./dev/,./proc/,./etc/init.d/,./sys/,./tmp/,./home/,./var/run/,./lib/firmware/ath9k_htc/}
         toucher "$TOUCH_TIME"
     printgr "BUSYBOX" "wget a welcome message"
-    wgetter "./welcome" "https://raw.githubusercontent.com/mikebdp2/floppinux-amd64net/refs/heads/main/welcome"
+    ### wgetter "./welcome" "https://raw.githubusercontent.com/mikebdp2/floppinux-amd64net/refs/heads/main/welcome"
         touch -h -t "$TOUCH_TIME" "./welcome"
     printgr "BUSYBOX" "create an inittab file"
     rm -f  ./etc/inittab
